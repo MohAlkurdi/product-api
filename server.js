1d97b2dea9c0d41c230d1d0d3f836a61fdd7a55d
mohalkurdi <mohxxkurdi@gmail.com>
2022-11-12 20:57:40 +0300

    add: .gitignore file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("product.json");
+const router = jsonServer.router("data.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.port || 3000;
 
